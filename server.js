3819f3ef868423d1eac8899e042b2d750a2b9c5e
DcodeWeb-51 <1scriptics@gmail.com>
2026-03-13 15:13:40 +0530

    backend db updated-3

--- server.js
+++ server.js
@@ -49,6 +49,8 @@ let resetOTP = {};
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
